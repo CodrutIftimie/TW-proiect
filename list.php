72320785938fea2fe938d8128b96c344f8110f12
Codrut Iftimie <codrut.iftimie@gmail.com>
2018-05-04 15:41:44 +0300

    Sorting for list.php
    -Working sorting for list.php (bug when you searched for something and change the order then the search disappears)
    -Other improvements

--- list.php
+++ list.php
@@ -1,9 +1,38 @@
 <?php
-session_start();
-include "database.php";
-if(!isset($_SESSION["activePageNumber"])) {
-    $_SESSION["activePageNumber"] = 1;
-}
+    if ( ! session_id() ) @ session_start();
+    include "database.php";
+
+    if(isset($_GET["search"])) {
+        $title = $_GET["search"];
+    } else $title = "";
+
+    if(isset($_GET["page"])) {
+        $page = $_GET["page"];
+    } else $page = 1;
+
+    if(isset($_GET["order"])) {
+        switch($_GET["order"]) {
+            case 1 : $order = "created_t DESC"; break;
+            case 2 : $order = "created_t ASC"; break;
+            case 3 : $order = "product_name ASC"; break;
+            case 4 : $order = "product_name DESC"; break;
+            default: $order = "created_t DESC"; break;
+        }
+    } else $order = "created_t DESC";
+
+    function setLink($pagen) {
+        $link = "";
+        if(isset($_GET["search"])) {
+            if(isset($_GET["order"]))
+                $link = "list.php?search=" . $_GET["search"] . "&page=" . $pagen . "&order=" . $_GET["order"];
+            else $link = "list.php?search=" . $_GET["search"] . "&page=" . $pagen;
+        }
+        else if(isset($_GET["order"]))
+        $link = "list.php?page=" . $pagen . "&order=" . $_GET["order"];
+        else $link = "list.php?page=" . $pagen;
+        return $link;
+    }
+
 ?>
 
 <html>
@@ -18,6 +47,34 @@ if(!isset($_SESSION["activePageNumber"])) {
     <link href="css/listStyle.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:500|Permanent+Marker|Fugaz+One" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Cabin|Pacifico|Mina:700|Inconsolata|Montseratt|Farsan" rel="stylesheet">
+
+    <script>
+    function search(e){
+        if(e.keyCode === 13){
+            e.preventDefault();
+            var link="list.php?search=" + document.getElementById('searchbar').value;
+            window.location.assign(link);
+        }
+    }
+
+    function changeOrder() {
+        var url = window.location.pathname;
+        var val = document.getElementById("order").value;
+        if(url.indexOf("?search=") != -1)
+            if(url.indexOf("&page=") != -1)
+                if(url.indexOf("&order=") != -1)
+                    window.location.assign(url.slice(0,-1) + val);
+                else window.location.assign(url + "&order=" + val); 
+            else window.location.assign(url + "&order=" + val); 
+        else if(url.indexOf("?page=") != -1) 
+            if(url.indexOf("&order=") != -1)
+                window.location.assign(url.slice(0,-1) + val); 
+            else window.location.assign(url + "&order=" + val); 
+        else if(url.indexOf("?order=") != -1)
+            window.location.assign(url.slice(0,-1) + val);
+        else window.location.assign(url + "?order=" + val);
+    }
+    </script>
 </head>
 
 <body>
@@ -32,9 +89,15 @@ if(!isset($_SESSION["activePageNumber"])) {
                 </a>
             </div>
         </div>
-        <div id="search">
-            <input type="text" placeholder="&#x1F50E; Search for a product...">
-        </div>
+        <form id="search">
+            <?php
+                if($title!="") {
+                    $holder = "&#x1F50E; " . $title;
+                }
+                else $holder = "&#x1F50E; Search for a product...";
+                echo '<input id="searchbar" type="text" placeholder="' . $holder . '" onkeypress="search(event)">';
+            ?>
+        </form>
         <div id="navMenu">
             <div id="menubutton"></div>
             <a href="index.html">
@@ -64,6 +127,9 @@ if(!isset($_SESSION["activePageNumber"])) {
             </div>
             <div>
                 <h5>Ingredients</h5>
+                <input type="checkbox" value="Water" id="water">
+                <label for="water">Water</label>
+                <br>
                 <input type="checkbox" value="Tomatoes" id="tomatoes">
                 <label for="tomatoes">Tomatoes</label>
                 <br>
@@ -122,43 +188,48 @@ if(!isset($_SESSION["activePageNumber"])) {
         </div>
         <div id="items">
             <h6>Sort by</h6>
-            <select>
-                <option>Newest</option>
-                <option>Price ascending</option>
-                <option>Price descending</option>
-                <option>Name A - Z</option>
-                <option>Name Z - A</option>
+            <select id="order" onchange="changeOrder()">
+                <?php 
+                    echo '<option value="1" ' . ($order=="created_t DESC"?"selected":"") .'>Newest</option>';
+                    echo '<option value="2" ' . ($order=="created_t ASC"?"selected":"") .'>Oldest</option>';
+                    echo '<option value="3" ' . ($order=="product_name ASC"?"selected":"") .'>Name A - Z</option>';
+                    echo '<option value="4" ' . ($order=="product_name DESC"?"selected":"") .'>Name Z - A</option>';
+                ?>
             </select>
             <ul>
                 <?php
-                    $query = "SELECT image,title,in_stock,FLOOR(price),SUBSTRING_INDEX(price,'.',-1),link FROM products LIMIT 16";
-                    $result = mysqli_query($connection, $query);
+                    $query = 'SELECT image_url,product_name,10,19,99,url,code FROM products_test WHERE LOWER(product_name) LIKE \'%' . strtolower($title) . '%\' ORDER BY ' . $order;
+                    $queryFinal = $query . ' LIMIT ' . (($page-1)*16) . ',16';
+                    $result = mysqli_query($connection, $queryFinal);
                     while($row = mysqli_fetch_row($result)) {
                         echo '<li>
-                                <img src="' . $row[0] . '">
-                                <h1>' . $row[1] . '</h1>
+                                <img src="' . $row[0] . '" alt="images/missing.png">
+                                <h1>' . ($row[1]!=""?$row[1]:$row[6]) . '</h1>
                                 <h2>In stock (' . $row[2] . ')</h2>
                                 <h3>$' . $row[3] . '<sup>' . $row[4] . '</sup></h3>
                                 <form action="' . $row[5] . '"><input type="submit" value="Details"></form>
-                            </li>';
+                              </li>';
                     }
                 ?>
             </ul>
             <div id="pageSelect">
-                <a href="#">Previous</a>
                 <?php
-                    $query = "SELECT FLOOR(COUNT(*)/16)+2 FROM products ORDER BY created_at DESC";
-                    $row = mysqli_fetch_row(mysqli_query($connection, $query));
+                    if($page>1)
+                        echo '<a href="' . setLink($page-1) . '" target="_self">Previous</a>';
+                    $numrows = "SELECT FLOOR(COUNT(*)/16)+2 FROM (" . $query . ") as t";
+                    $row = mysqli_fetch_row(mysqli_query($connection, $numrows));
                     for($i=1;$i<$row[0];$i++) {
-                        if($i != $_SESSION["activePageNumber"]) {
-                            echo '<a href="#">' . $i . '</a>';
+                        if($i != $page) {
+
+                            echo '<a href="' . setLink($i) . '" target="_self">' . $i . '</a>';
                         }
                         else {
-                            echo '<a href="#" class="active">' . $i . '</a>';
+                            echo '<a href="' . setLink($i) . '" class="active" target="_self">' . $i . '</a>';
                         }
                     }
+                    if($page<$row[0]-1)
+                        echo '<a href="' . setLink($page+1) . '" target="_self">Next</a>';
                 ?>
-                <a href="#">Next</a>
             </div>
         </div>
     </div>
